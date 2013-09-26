14b9692d9a47b517736682e5682cc3fbbe6f5b97
Patrick Ryan <phiat99@gmail.com>
2013-09-26 17:36:06 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject oeis-clj "0.1.0-SNAPSHOT"
   :description "A Clojure library for querying sequences on oeis.org"
-  :url "fix"
+  :url "https://github.com/phiat/oeis-clj/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
